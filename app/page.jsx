c34687f2301d2d9bee5bcff4bfbff94b09943b8d
theWebCookie <jabwojtek@wp.pl>
2023-11-17 23:15:52 +0100

    refactor: change body display

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,10 @@
 import Hero from './ui/main/hero/hero';
-import styles from './page.module.css';
 import MainSection from './ui/main/main-section/main-section';
 import CarSection from './ui/main/car-section/car-section';
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main>
       <Hero />
       <MainSection />
       <CarSection />
